19e007680cd2f829e70f4019c75ea5bac9923aab
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-04-27 23:37:33 +0200

    nie trzeba zaokrągląc bo zawsze są całkowite

--- mn.js
+++ mn.js
@@ -2,21 +2,16 @@
 // Write a function that accepts two arguments and generates a sequence containing the integers from the first argument to the second inclusive.
 
 function generateIntegers(m, n) {
-  const mm = Math.ceil(m);
-  const nn = Math.ceil(n);
-  //const dlugoscTablicy=nn-mm;
-  const liczenie = new Array(nn - mm + 1);
-  liczenie[0] = mm;
-  liczenie[liczenie.length - 1] = nn;
+  
+  const liczenie = new Array(n - m + 1);
 
   for (let i = 0; i <= liczenie.length - 1; i++) {
-    liczenie[i] = mm + i;
+    liczenie[i] = m + i;
   }
 
   //return liczenie;
-  console.log(mm);
-  console.log(nn);
+
   console.log(liczenie);
 }
 
-generateIntegers(19.7, 71.9);
+generateIntegers(19, 71);
